21575136f4e0b890aaf31d7a0467e00eb3be55af
joegasewicz <joegasewicz@gmail.com>
2022-02-27 17:33:52 +0000

    init

---
+++ easy_events.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Joe Geezer on 27/02/2022.
+//
+
+#include "easy_events.h"
---
+++ easy_events.h
@@ -0,0 +1,14 @@
+//
+// Created by Joe Geezer on 27/02/2022.
+//
+
+#ifndef TICKET_MANAGER_EASY_EVENTS_H
+#define TICKET_MANAGER_EASY_EVENTS_H
+
+
+class easy_events {
+
+};
+
+
+#endif //TICKET_MANAGER_EASY_EVENTS_H
